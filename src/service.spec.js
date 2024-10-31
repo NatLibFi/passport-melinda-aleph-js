ce43d8de46bec037c3ed8625341a6eb6836c98d6
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-10-31 13:58:41 +0200

    Change license to MIT

--- src/service.spec.js
+++ src/service.spec.js
@@ -1,32 +1,3 @@
-/* eslint-disable no-console */
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Passport authentication strategy for Melinda using Aleph credentials
-*
-* Copyright (C) 2018-2022 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of passport-melinda-aleph-js
-*
-* passport-melinda-aleph-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* passport-melinda-aleph-js is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
-
 import fs from 'fs';
 import path from 'path';
 import {expect} from 'chai';
